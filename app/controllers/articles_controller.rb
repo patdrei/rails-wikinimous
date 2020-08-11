b3fe65405804c6f3a9c59a809acae3ccce8f3772
Patrick Dreier <patrick3dreier@googlemail.com>
2020-08-11 15:56:41 +0200

    tis done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index # Read all
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(strong_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(strong_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  Article.create!(
+    title: Faker::Movies::HarryPotter.character,
+    content: Faker::Movies::HarryPotter.quote
+    )
+end
